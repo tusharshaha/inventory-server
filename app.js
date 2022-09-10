e75d461423db7d04cd28d85204badd49469643d2
tusharshaha <tusharshaha128@gmail.com>
2022-09-10 20:56:18 +0600

    created product schema

--- app.js
+++ app.js
@@ -14,9 +14,55 @@ const productSchema = mongoose.Schema({
         required: [true, "Please provide a name"],
         trim: true,
         unique: true,
-        minLength: [3, "Name must be at least 3 character"]
-    }
+        minLength: [3, "Name must be at least 3 character"],
+        maxLength: [100, "Name too large"]
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, "Price can't be negative"]
+    },
+    unit: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["kg", "litre", "pcs"],
+            message: "Unit value can't be {VALUE}, must be kg/litre/pcs"
+        }
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: [0, "Quantity can't be negative"]
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["in-stock", "out-of-stock", "discontinued"],
+            message: "Unit value can't be {VALUE}, must be in-stock/out-of-stock/discontinued"
+        }
+    },
+    supplier: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Supplier"
+    },
+    categories: [{
+        name: {
+            type: String,
+            required: true,
+        },
+        _id: mongoose.Schema.Types.ObjectId
+    }]
+}, {
+    timestamps: true
 })
+
+
 
 // check server
 app.get("/", (req, res) => {
